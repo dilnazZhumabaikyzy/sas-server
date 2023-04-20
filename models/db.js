755b6a33176e06ffd781be89d5068892de66ccef
inspiredsamat <108996318+inspiredsamat@users.noreply.github.com>
2023-04-20 14:10:47 +0600

    db config bug solves

--- models/db.js
+++ models/db.js
@@ -1,20 +1,23 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres',
-  });
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: 'postgres',
+});
 
 async function initialize() {
-    try {
-      await sequelize.authenticate();
-      await sequelize.sync();
-      // sequelize.clearCache();
-      console.log("TEST TEST TEST TEST!!!!!!!!!!!!!!!!!!!!!!!!");
-      // console.log(sequelize);
-      console.log('Database connection successful');
-    } catch (error) {
-      console.error('Error connecting to database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    // sequelize.clearCache();
+    console.log("TEST TEST TEST TEST!!!!!!!!!!!!!!!!!!!!!!!!");
+    // console.log(sequelize);
+    console.log('Database connection successful');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
   }
-  export { sequelize, initialize };
+}
+export { sequelize, initialize };
