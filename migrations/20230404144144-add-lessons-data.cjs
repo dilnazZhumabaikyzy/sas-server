434a2c931f8317c075af9721fb9c19736f533bac
inspiredsamat <108996318+inspiredsamat@users.noreply.github.com>
2023-05-08 20:19:51 +0600

    shortenes week days

--- migrations/20230404144144-add-lessons-data.cjs
+++ migrations/20230404144144-add-lessons-data.cjs
@@ -7,7 +7,7 @@ module.exports = {
           lessonName: "Low-level Architecture",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Monday 09:00',
+          time: 'Mon 09:00',
           group: '02N',
           room: 'H302',
           courseId: 'CSS 309'
@@ -16,7 +16,7 @@ module.exports = {
           lessonName: "Natural Language Processing",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Monday 11:00',
+          time: 'Mon 11:00',
           group: '02N',
           room: 'F202',
           courseId: 'INF 376'
@@ -25,7 +25,7 @@ module.exports = {
           lessonName: "Software Engineering",
           lessonType: 'Lecture',
           teacherId: 200107112,
-          time: 'Monday 12:00',
+          time: 'Mon 12:00',
           group: '01N',
           room: 'E221',
           courseId: 'CSS 342'
@@ -34,7 +34,7 @@ module.exports = {
           lessonName: "Cyber Security",
           lessonType: 'Practice',
           teacherId: 200107112,
-          time: 'Monday 14:00',
+          time: 'Mon 14:00',
           group: '02N',
           room: 'A102',
           courseId: 'CSS 306'
@@ -43,7 +43,7 @@ module.exports = {
           lessonName: "Game Physics",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Monday 16:00',
+          time: 'Mon 16:00',
           group: '01N',
           room: 'D214',
           courseId: 'INF 261'
@@ -52,7 +52,7 @@ module.exports = {
           lessonName: "Software Engineering",
           lessonType: 'Lecture',
           teacherId: 200107112,
-          time: 'Tuesday 12:00',
+          time: 'Tue 12:00',
           group: '02N',
           room: 'D221',
           courseId: 'CSS 342'
@@ -61,7 +61,7 @@ module.exports = {
           lessonName: "Distributed Computing",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: "Tuesday 11:00",
+          time: "Tue 11:00",
           group: '01N',
           room: 'C102',
           courseId: 'CSS 440'
@@ -70,7 +70,7 @@ module.exports = {
           lessonName: "Psychology",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: "Tuesday 12:00",
+          time: "Tue 12:00",
           group: '15N',
           room: 'D201',
           courseId: 'MDE 154'
@@ -79,7 +79,7 @@ module.exports = {
           lessonName: "Natural Language Processing",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Tuesday 14:00',
+          time: 'Tue 14:00',
           group: '01N',
           room: 'D202',
           courseId: 'INF 376'
@@ -88,7 +88,7 @@ module.exports = {
           lessonName: "Advanced computer architecture",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Tuesday 16:00',
+          time: 'Tue 16:00',
           group: '01N',
           room: 'F102',
           courseId: 'CSS 345'
@@ -97,7 +97,7 @@ module.exports = {
           lessonName: "Game Physics",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Wednesday 09:00',
+          time: 'Wed 09:00',
           group: '02N',
           room: 'F201',
           courseId: 'INF 261'
@@ -106,7 +106,7 @@ module.exports = {
           lessonName: "Cultural Studies",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Wednesday 11:00',
+          time: 'Wed 11:00',
           group: '11N',
           room: 'C103',
           courseId: 'MDE 153'
@@ -115,7 +115,7 @@ module.exports = {
           lessonName: "Advanced computer architecture",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Wednesday 13:00',
+          time: 'Wed 13:00',
           group: '04N',
           room: 'F301',
           courseId: 'CSS 345'
@@ -124,7 +124,7 @@ module.exports = {
           lessonName: "Psychology",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: "Wednesday 14:00",
+          time: "Wed 14:00",
           group: '12N',
           room: 'D202',
           courseId: 'MDE 154'
@@ -133,7 +133,7 @@ module.exports = {
           lessonName: "Computer Vision",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Wednesday 16:00',
+          time: 'Wed 16:00',
           group: '01N',
           room: 'F203',
           courseId: 'INF 324'
@@ -142,7 +142,7 @@ module.exports = {
           lessonName: "Low-level Architecture",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Thursday 10:00',
+          time: 'Thu 10:00',
           group: '03N',
           room: 'H302',
           courseId: 'CSS 309'
@@ -151,7 +151,7 @@ module.exports = {
           lessonName: "Distributed Computing",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Thursday 12:00',
+          time: 'Thu 12:00',
           group: '02N',
           room: 'D102',
           courseId: 'CSS 440'
@@ -160,7 +160,7 @@ module.exports = {
           lessonName: "Advanced computer architecture",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Thursday 14:00',
+          time: 'Thu 14:00',
           group: '03N',
           room: 'D302',
           courseId: 'CSS 345'
@@ -169,7 +169,7 @@ module.exports = {
           lessonName: "Natural Language Processing",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Thursday 16:00',
+          time: 'Thu 16:00',
           group: '02N',
           room: 'D202',
           courseId: 'INF 376'
@@ -178,7 +178,7 @@ module.exports = {
           lessonName: "Cultural Studies",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Thursday 17:00',
+          time: 'Thu 17:00',
           group: '12N',
           room: 'C103',
           courseId: 'MDE 153'
@@ -187,7 +187,7 @@ module.exports = {
           lessonName: "Distributed Computing",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: "Friday 09:00",
+          time: "Fri 09:00",
           group: '02N',
           room: 'D102',
           courseId: 'CSS 440'
@@ -196,7 +196,7 @@ module.exports = {
           lessonName: "Natural Language Processing",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Friday 10:00',
+          time: 'Fri 10:00',
           group: '01N',
           room: 'F202',
           courseId: 'INF 376'
@@ -205,7 +205,7 @@ module.exports = {
           lessonName: "Cultural Studies",
           lessonType: 'Practice',
           teacherId: 200107111,
-          time: 'Friday 11:00',
+          time: 'Fri 11:00',
           group: '03N',
           room: 'F102',
           courseId: 'MDE 153'
@@ -214,7 +214,7 @@ module.exports = {
           lessonName: "Software Engineering",
           lessonType: 'Lecture',
           teacherId: 200107112,
-          time: 'Friday 12:00',
+          time: 'Fri 12:00',
           group: '03N',
           room: 'E221',
           courseId: 'CSS 342'
@@ -223,7 +223,7 @@ module.exports = {
           lessonName: "Low-level Architecture",
           lessonType: 'Lecture',
           teacherId: 200107111,
-          time: 'Friday 16:00',
+          time: 'Fri 16:00',
           group: '01N',
           room: 'H303',
           courseId: 'CSS 309'
